7eaff3347943f02a67bb41261e4472783ed02727
Unu-Undrakh Zorigtbaatar <unu-undrakhzorigtbaatar@Mac.fritz.box>
2025-11-10 17:25:59 +0100

    assignment is done

--- breakdown_time.py
+++ breakdown_time.py
@@ -1,4 +1,14 @@
 def breakdown_time(seconds):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if not isinstance(seconds, int) or seconds < 0:
+        return -1
+   
+    if seconds == 0:
+        return {}
+  
+    units = [3600, 60, 1] 
+    
+    return {u: v for u in units
+            for v in [seconds // u if u == 3600 else (seconds % 3600) 
+            // 60 if u == 60 else seconds % 60] if v !=0}
+
+    
--- swap_ends.py
+++ swap_ends.py
@@ -1,4 +1,9 @@
 def swap_ends(L, k):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if not isinstance(L, list) or k <= 0 or len(L) == 0 or k > len(L) // 2:
+        return (L.copy(), 0)
+    new_list = L.copy()
+    new_list[:k], new_list[-k:] = L[-k:], L[:k]
+    num_swaps = k
+    return (new_list, num_swaps)
+    
+    
